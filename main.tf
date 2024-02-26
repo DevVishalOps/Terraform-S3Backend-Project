df0ed1bb65ff39c441d8f046e69f00e90d39d130
vishal <rajput.vishal.rajes@gmail.com>
2024-02-26 17:52:48 +0530

    resourse_file_update1

--- main.tf
+++ main.tf
@@ -7,3 +7,30 @@ resource "aws_s3_bucket" "mybucket" {
     Environment = "Dev"
   }
 }
+
+resource "aws_s3_bucket_ownership_controls" "bucket" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "bucket" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.bucket,
+    aws_s3_bucket_public_access_block.bucket,
+  ]
+
+  bucket = aws_s3_bucket.mybucket.id
+  acl    = "public-read"
+}
